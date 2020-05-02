1f78d935795435025f14e81f40404d4b771153e8
himanshu2330 <him.shekhar89@gmail.com>
2020-05-02 10:45:40 -0700

    update file

--- workload_migration.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /opt/git_repo/ansible-tower-workload
-rm -rf workload-migration-new  
-git clone https://github.com/himanshu2330/workload-migration-new.git 
-$(cd /opt/git_repo/ansible-tower-workload/workload-migration-new/ ; sh start_workload_migration.sh)
